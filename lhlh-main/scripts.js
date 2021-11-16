f82ad66a4b3ee5a20ecb1bc153caf99b5381e696
gigigiy <93245129+gigigiy@users.noreply.github.com>
2021-11-16 16:57:48 +0600

    Update scripts.js

--- lhlh-main/scripts.js
+++ lhlh-main/scripts.js
@@ -2,7 +2,7 @@ function login() {
     const element = document.getElementById("name").value;
     const password = document.getElementById("pass").value;
     if (element == "Timur" && password == "1234") {
-        window.location.href = "https://lopl-html.herokuapp.com/"
+        window.location.href = "https://port-timur.herokuapp.com/"
     } else {
         alert("Wrong")
     }
